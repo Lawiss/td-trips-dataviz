46712e4bae541920818a04b293a4a7de32d00ba1
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-10-31 15:00:53 +0100

    Changes mapstyle

--- app.jsx
+++ app.jsx
@@ -44,7 +44,7 @@ const INITIAL_VIEW_STATE = {
   bearing: 0
 };
 
-const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
+const MAP_STYLE = 'https://tiles.basemaps.cartocdn.com/gl/voyager-gl-style/style.json';
 
 const landCover = [
   [
