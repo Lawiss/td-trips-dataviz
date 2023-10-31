33422694456fcb92d64666746a6cc049138b66c6
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-10-31 16:33:24 +0100

    Adds a way to pause animation

--- app.jsx
+++ app.jsx
@@ -3,38 +3,11 @@ import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Map } from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
-import { AmbientLight, PointLight, LightingEffect } from '@deck.gl/core';
 import DeckGL from '@deck.gl/react';
-import { PolygonLayer } from '@deck.gl/layers';
 import { TripsLayer } from '@deck.gl/geo-layers';
 
-const ambientLight = new AmbientLight({
-  color: [255, 255, 255],
-  intensity: 1.0
-});
 
-const pointLight = new PointLight({
-  color: [255, 255, 255],
-  intensity: 2.0,
-  position: [-74.05, 40.7, 8000]
-});
 
-const lightingEffect = new LightingEffect({ ambientLight, pointLight });
-
-const material = {
-  ambient: 0.1,
-  diffuse: 0.6,
-  shininess: 32,
-  specularColor: [60, 64, 70]
-};
-
-const DEFAULT_THEME = {
-  buildingColor: [74, 80, 87],
-  trailColor0: [253, 128, 93],
-  trailColor1: [23, 184, 190],
-  material,
-  effects: [lightingEffect]
-};
 
 const INITIAL_VIEW_STATE = {
   longitude: 2.2882695,
@@ -46,68 +19,52 @@ const INITIAL_VIEW_STATE = {
 
 const MAP_STYLE = 'https://tiles.basemaps.cartocdn.com/gl/voyager-gl-style/style.json';
 
-const landCover = [
-  [
-    [-74.0, 40.7],
-    [-74.02, 40.7],
-    [-74.02, 40.72],
-    [-74.0, 40.72]
-  ]
-];
+
 
 export default function App({
-  trailLength = 500,
+  trailLength = 2000,
   initialViewState = INITIAL_VIEW_STATE,
   mapStyle = MAP_STYLE,
-  theme = DEFAULT_THEME,
   animationSpeed = 25
 }) {
   const [time, setTime] = useState(1698278400);
   const [animation] = useState({});
-  const [currentDateString, setCurrentDateString] = useState('');
+  const [isPlaying, setIsPlaying] = useState(false); // To track whether the animation is playing or paused
+  const [displayTime, setDisplayTime] = useState(time);
 
   const animate = () => {
-    setTime(t => {
-      //console.log("This is t :", t)
+    setTime((t) => {
       let new_t;
       if (t > 1698410761.781) {
-        new_t = 1698278400
+        new_t = 1698278400;
       } else {
-        new_t = t + animationSpeed
+        new_t = t + (isPlaying ? animationSpeed : 0); // Only advance time if playing
       }
-      setCurrentDateString(new Date(new_t * 1000).toLocaleString());
-      //console.log(new_t)  
-      return new_t
+      setDisplayTime(new_t); // Update displayTime
+      return new_t;
     });
     animation.id = window.requestAnimationFrame(animate);
   };
 
+
+
   useEffect(() => {
     animation.id = window.requestAnimationFrame(animate);
     return () => window.cancelAnimationFrame(animation.id);
-  }, [animation]);
+  }, [isPlaying]);
 
   const layers = [
-    // This is only needed when using shadow effects
-    new PolygonLayer({
-      id: 'ground',
-      data: landCover,
-      getPolygon: f => f,
-      stroked: false,
-      getFillColor: [0, 0, 0, 0]
-    }),
     new TripsLayer({
       id: 'trips',
       data: "https://raw.githubusercontent.com/Lawiss/td-trips-dataviz/main/public/trips.json",
       getPath: d => d.coordinates,
       getTimestamps: d => d.timestamps,
-      getColor: d => theme.trailColor1,
+      getColor: d => [23, 184, 190],
       opacity: 1,
       widthMinPixels: 3,
       rounded: true,
-      trailLength,
-      currentTime: time,
-
+      trailLength: trailLength,
+      currentTime: time, // Use sliderValue when paused
       shadowEnabled: false
     })
   ];
@@ -116,14 +73,30 @@ export default function App({
     <div>
       <DeckGL
         layers={layers}
-        effects={theme.effects}
         initialViewState={initialViewState}
         controller={true}
       >
         <Map reuseMaps mapLib={maplibregl} mapStyle={mapStyle} preventStyleDiffing={true} />
       </DeckGL>
       <div style={{ position: 'absolute', top: '10px', left: '10px', background: 'rgba(255, 255, 255, 0.8', padding: '10px', borderRadius: '5px' }}>
-        {currentDateString}
+        {new Date(displayTime * 1000).toLocaleString()}
+        <button onClick={() => {
+          setIsPlaying(!isPlaying)
+        }}>
+          {isPlaying ? 'Pause' : 'Play'}
+        </button>
+        <input
+          type="range"
+          min={1698278400}
+          max={1698410761.781}
+          step={animationSpeed}
+          value={time}
+          onChange={(e) => {
+            const newTime = parseFloat(e.target.value);
+            setTime(newTime);
+            setDisplayTime(newTime); // Update displayTime
+          }}
+        />
       </div>
     </div>
   );
